bc05ebad1f415259dffae0dc9ead3fc4ffc3361b
eslam-ashraf <43147649+eslam-ashraf@users.noreply.github.com>
2020-03-02 21:31:33 -0800

    Add files via upload

--- js/my_script.js
+++ js/my_script.js
@@ -1,1 +1,37 @@
 
+$(document).ready(function(){
+    //start of show cars car form function
+   setInterval(function(){
+        update_questions();
+    } , 1000);
+    
+    function update_questions(){
+        $.ajax({
+           url:'show_questions.php',
+           type:'POST',
+           success:function(show_questions){
+               if(!show_questions.error){
+                   $("#show-questions").html(show_questions);
+               }
+           }    
+        });
+    }
+    
+    //start of add car form function 
+    
+    $('#add_question_form').submit(function(e){
+                              
+        e.preventDefault();
+        
+        var postData = $(this).serialize();
+        
+        var url = $(this).attr('action');
+         
+        $.post(url , postData , function(show_questions){
+           
+            $("#show-cars").html(show_questions);
+        });
+                              
+    });
+    //end of add car form function 
+}
